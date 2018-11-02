83d8b37f09251eab752afe0bddf8f051359df598
cml97 <cmluzunhasan@gmail.com>
2018-11-02 10:39:54 +0300

    API KEY deleted

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,33 +2,12 @@ import * as firebase from 'firebase';
 import moment from 'moment';
 
 const config = {
-    apiKey: "AIzaSyB5Bmte_L_ABgifI7i0Todn41HXfGSrE_I",
-    authDomain: "expensify-app-e5dad.firebaseapp.com",
-    databaseURL: "https://expensify-app-e5dad.firebaseio.com",
-    projectId: "expensify-app-e5dad",
-    storageBucket: "expensify-app-e5dad.appspot.com",
-    messagingSenderId: "872847996280"
+    
 };
 firebase.initializeApp(config);
 
 const database = firebase.database();
-// database.ref('person').set({
-//   name: 'Cemil Uzunhasan',
-//   height: 187,
-//   weight: 64,
-//   age: 21,
-//   stressLevel: 6,
-//   job: {
-//     title: 'Computer Engineer',
-//     company: 'Bilgetech'
-//   },
-//   location: {
-//     province: 'Istanbul',
-//     country: 'Turkey'
-//   }
-// }).then(() => {
-//   console.log("Save the value");
-// }).catch((err) => console.log("Error var ", err));
+
 
 const expenses = [{
   note: 'Car Rent',
@@ -51,12 +30,3 @@ const expenses = [{
 
 database.ref("expenses").set(expenses);
 database.ref().once('value').then(snapshot => console.log(snapshot.val()));
-
-// const promise = new Promise((resolve, reject) => {
-//   resolve('anan');
-//   reject("Hata anan");
-// });
-//
-// const req = promise
-// .then((data) => console.log("Data isss", data))
-// .catch((err) => console.log("Error is ", err))
